82d75232d3bab9e2b5aa5e85ff15c2a943399360
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-05-29 18:34:13 +0200

    Combined tuning test scripts into one

--- tests/tuning/tuning_regressor.py
+++
@@ -1,8 +0,0 @@
-from src.data.cf_classification import CF_Classification
-from src.data.cf import CF
-from src.utilities.tuning import optimize_hyperparameters
-
-from src.models.downstream.classifier import TimeseriesClassifier, train_classifier
-
-if __name__ == "__main__":
-    pass
