23c5aba5066dd951589c42580b6249e6f9d8662b
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-05-29 18:33:20 +0200

    implemented adaptive model training function

--- src/models/models.py
+++ src/models/models.py
@@ -1,21 +1,18 @@
 from src.models.downstream.regressor import TimeseriesRegressor, train_regressor
 from src.models.downstream.classifier import TimeseriesClassifier, train_classifier
 
-from src.models.generative.rgan import RGAN, train_RGAN, RGAN_params
-from src.models.generative.rwgan import RWGAN, train_RWGAN, RWGAN_params
-from src.models.generative.timegan import TimeGAN, train_TimeGAN, TimeGAN_params
+from src.models.generative.rgan import RGAN, train_RGAN
+from src.models.generative.rwgan import RWGAN, train_RWGAN
+from src.models.generative.timegan import TimeGAN, train_TimeGAN
 
-utility_models = {
-    "regressor": (TimeseriesRegressor, train_regressor),
-    "classifier": (TimeseriesClassifier, train_classifier)
-}
-
-gen_models = {
-    "RGAN": (RGAN, train_RGAN, RGAN_params),
-    "RWGAN": (RWGAN, train_RWGAN, RWGAN_params),
-    "TimeGAN": (TimeGAN, train_TimeGAN, TimeGAN_params)
-}
-
-if __name__ == "__main__":
-    print(gen_models)
-    print(utility_models)
+def train_model(model, train_data, log_dir, val_data=None):
+    if isinstance(model, RGAN):
+        return train_RGAN(model, train_data, log_dir)
+    elif isinstance(model, TimeGAN):
+        return train_TimeGAN(model, train_data, log_dir, val_data)
+    elif isinstance(model, RWGAN):
+        return train_RWGAN(model, train_data, log_dir)
+    elif isinstance(model, TimeseriesClassifier):
+        return train_classifier(model, train_data, log_dir, val_data)
+    elif isinstance(model, TimeseriesRegressor):
+        return train_regressor(model, train_data, log_dir, val_data)
