96559aa2d1043d7d94e0b35ea6f2d5d1d8efa0a3
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-05-29 13:11:12 +0200

    updated regressor and classifier testing script

--- tests/models/downstream/classifier.py
+++ tests/models/downstream/classifier.py
@@ -1,22 +1,50 @@
+from src.data.data_loader import select_data
+
 from src.models.downstream.classifier import TimeseriesClassifier, train_classifier
 from src.data.cf_classification import CF_Classification
 from src.models.downstream.downsteam_model import downstream_model_params
-from src.data.cf import CF
+
 import torch
+from torch.utils.data import random_split
+
 
 if __name__ == "__main__":
+    # Load real data
+    dataset = select_data("cf_classification")
+    cf_class = CF_Classification(dataset.sequences, dataset.columns.copy())
+    numseq, numev, numfeat = cf_class.sequences.shape
+
+    # Load synthetic data
+    syndata = torch.load(f"outputs/testing/genmodels/RGAN/syndata")
+    cf_syn = CF_Classification(syndata, dataset.columns.copy())
+    assert len(cf_class) == len(cf_syn)
+
+    # calculate splitsizes
+    train_size = int(0.8* len(cf_class))
+    val_size = int(len(dataset) - train_size)
+    splits = [train_size, val_size]
+
+    # Split datasets
+    train_real, val_real = random_split(cf_class, splits)
+    train_syn, val_syn = random_split(cf_syn, splits)
+
+    # Update parameters
+    downstream_model_params.update({
+        "num_sequences": numseq,
+        "num_events": numev,
+        "num_features": numfeat
+    })
+
     # Train model on real
-    cf = CF()
-    cf_class = CF_Classification(cf.sequences, cf.columns.copy())
-    model = TimeseriesClassifier(cf_class.sequences.shape, **downstream_model_params)
-    train_classifier(model, cf_class, f"outputs/testing/classifier/2")
+    model = TimeseriesClassifier(**downstream_model_params)
+    model.output_path = f"outputs/testing/downstream/{model.__NAME__}/real"
+    train_classifier(model, train_real, val_real, log_dir=f"runs/testing/{model.__NAME__}/real")
 
     # Train model on synthetic
-    syndata = torch.load(f"outputs/testing/RGAN/syn.pt")
-    cf_syn = CF_Classification(syndata, cf.columns.copy())
-    model2 = TimeseriesClassifier(cf_syn.sequences.shape, **downstream_model_params)
-    train_classifier(model2, cf_syn, f"outputs/testing/classifier/2"
-)
+    model2 = TimeseriesClassifier(**downstream_model_params)
+    model2.output_path = f"outputs/testing/downstream/{model.__NAME__}/syn"
+
+    train_classifier(model2, train_syn, val_syn, log_dir=f"runs/testing/{model.__NAME__}/synthetic")
 
     
 
