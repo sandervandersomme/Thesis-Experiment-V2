af0d8cda74753d7e75d9cc822bdfa99b7ef90cf5
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-05-29 12:07:19 +0200

    Updated timegan testing script

--- tests/models/generation/timegan.py
+++ tests/models/generation/timegan.py
@@ -1,15 +1,26 @@
-from src.data.data_loader import get_data
+from src.data.data_loader import select_data
 from src.models.generative.timegan import TimeGAN, train_TimeGAN, TimeGAN_params
 
+import torch
 
 if __name__ == "__main__":
-    train_data, test_data, shape = get_data("cf_classification", 0.7)
+    # Load data
+    dataset = select_data("cf_classification")
+    numseq, numev, numfeat = dataset.sequences.shape
 
-    model = TimeGAN(shape, **TimeGAN_params)
+    # Load parameters
+    TimeGAN_params.update({
+        "num_sequences": numseq,
+        "num_events": numev,
+        "num_features": numfeat
+    })
 
-    path = f"outputs/testing/TimeGAN"
+    # Train model
+    model = TimeGAN(**TimeGAN_params)
+    model.output_path = f"outputs/testing/genmodels/"
+    train_TimeGAN(model, dataset, dataset, log_dir=f"runs/testing/{model.__NAME__}/")
 
-    train_TimeGAN(model, train_data, path)
-
-
-    
+    # Generate and save data
+    syndata = model.generate_data(numseq)
+    torch.save(syndata, f"{model.output_path}/{model.__NAME__}/syndata")
+    print("Saving data..")
