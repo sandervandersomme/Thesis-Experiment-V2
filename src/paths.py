bb741d22944c15b8abfa17b25ea2be9679bf6e62
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-01 14:10:21 +0200

    Added paths file

---
+++ src/paths.py
@@ -0,0 +1,8 @@
+
+TEST_PATH_HYPERPARAMS = "outputs/tests/hyperparams/"
+TEST_PATH_MODEL = "outputs/tests/models/"
+
+PATH_HYPERPARAMS = "outputs/hyperparams/"
+PATH_MODEL = "outputs/models/"
+
+PATH_EXP1 = "outputs/experiments/1/hyperparams/"
