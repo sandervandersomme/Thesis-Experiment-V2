9c2838fdbdc4d8f69a5b11a9fb0270f78d26eedf
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-01 14:14:36 +0200

    Added model selection function

--- src/models/models.py
+++ src/models/models.py
@@ -1,18 +1,29 @@
-from src.models.downstream.regressor import TimeseriesRegressor, train_regressor
-from src.models.downstream.classifier import TimeseriesClassifier, train_classifier
+from src.models.regressor import TimeseriesRegressor, train_regressor
+from src.models.classifier import TimeseriesClassifier, train_classifier
 
-from src.models.generative.rgan import RGAN, train_RGAN
-from src.models.generative.rwgan import RWGAN, train_RWGAN
-from src.models.generative.timegan import TimeGAN, train_TimeGAN
+from src.models.rgan import RGAN, train_RGAN
+from src.models.rwgan import RWGAN, train_RWGAN
+from src.models.timegan import TimeGAN, train_TimeGAN
 
-def train_model(model, train_data, log_dir, val_data=None):
+def train_model(model, train_data, log_run_dir: str, log_loss_dir: str, val_data=None):
     if isinstance(model, RGAN):
-        return train_RGAN(model, train_data, log_dir)
+        return train_RGAN(model, train_data, log_run_dir, log_loss_dir)
     elif isinstance(model, TimeGAN):
-        return train_TimeGAN(model, train_data, log_dir, val_data)
+        return train_TimeGAN(model, train_data, log_run_dir, log_loss_dir, val_data)
     elif isinstance(model, RWGAN):
-        return train_RWGAN(model, train_data, log_dir)
+        return train_RWGAN(model, train_data, log_run_dir, log_loss_dir)
     elif isinstance(model, TimeseriesClassifier):
-        return train_classifier(model, train_data, log_dir, val_data)
+        return train_classifier(model, train_data, log_run_dir, log_loss_dir, val_data)
     elif isinstance(model, TimeseriesRegressor):
-        return train_regressor(model, train_data, log_dir, val_data)
+        return train_regressor(model, train_data, log_run_dir, log_loss_dir, val_data)
+    
+models = {
+    "rgan": RGAN,
+    "rwgan": RWGAN,
+    "timegan": TimeGAN,
+    "classifier": TimeseriesClassifier,
+    "regressor": TimeseriesRegressor
+}
+
+def select_model(model: str):
+    return models[model]
