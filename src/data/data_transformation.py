5da57e95a2e65b17975da830f2f5f7026f95ed09
sandervandersomme <57229133+sandervandersomme@users.noreply.github.com>
2024-06-05 12:35:44 +0200

    Added data transformation functions

---
+++ src/data/data_transformation.py
@@ -0,0 +1,17 @@
+import torch
+import numpy as np
+
+def flatten_into_sequences(data: torch.Tensor):
+    """
+    Transforms data with shape (sequences, events, features) into date with shape (sequences, features*events)
+    Note: Each sequence now is flattened such that different features describe different time-steps"""
+    sequences, events, features = data.shape
+    return data.reshape(sequences, events*features)
+
+def flatten_into_events(data: torch.Tensor):
+    """
+    Transforms data with shape (sequences, events, features) into an eventlog with shape (events, features)
+    Note: This removes temporal ordering of events
+    """
+    _, _, features = data.shape
+    return data.reshape(-1, features)
