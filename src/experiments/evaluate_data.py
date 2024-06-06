2fb6d47313e79cc4733b0a275fb9e0f6f23d2edf
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-06 21:56:20 +0200

    created evaluation script

--- src/experiments/evaluate_data.py
+++ src/experiments/evaluate_data.py
@@ -1,8 +1,19 @@
+import argparse
 
+from src.data.data_loader import load_syn_data
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--num_samples')
+    parser.add_argument('--exp', type=int, help="what experiment number?")
+    args = parser.parse_args()
+
     # Load synthetic dataset
+    path = f"outputs/syndata/{args.exp}/{args.dataset}-{args.model}-{args.num_samples}.pt"
+    dataset = load_syn_data(path)
     # Create evaluator
     # evaluate
     # save results
