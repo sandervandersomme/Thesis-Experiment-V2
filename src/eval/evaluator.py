19f71fa250f50d8f68dc76e037116f2c55a9d753
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-10 22:15:50 +0200

    Updated evaluator function

--- src/eval/evaluator.py
+++ src/eval/evaluator.py
@@ -1,50 +1,81 @@
+import pandas as pd
 import torch
 import inspect
+import argparse
 
+from src.eval.methods import similarity_methods, diversity_methods, privacy_methods, utility_methods, all_methods
+from src.models.models import GenModel
+
+import pandas as pd
 from typing import List, Callable
 
+import os
+
 class Evaluator:
-    def __init__(self, methods: List[Callable]):
+    def __init__(self, train_data: torch.Tensor, test_data: torch.Tensor, name_dataset: str, methods: List[Callable], **kwargs):
+        self.results = pd.DataFrame()
+        self.name_dataset = name_dataset
         self.methods = methods
-        self.results = {}
+        self.kwargs = kwargs
+        self.kwargs.update({
+            "train_data": train_data,
+            "test_data": test_data
+        })
 
-    def evaluate(self, **kwargs):
-        # Check if all arguments are 
-        required_params = set()
+        self.check_params_is_complete()
+
+    def evaluate_dataset(self, syndata: torch.Tensor, model: GenModel):
+        self.kwargs.update({
+            "syndata": syndata,
+            "model" : model
+        })
+
+        scores = {"Name dataset": self.name_dataset}
+
         for method in self.methods:
-            required_params.update(inspect.signature(method).parameters)
-
-        # Assert that all required parameters are present in kwargs
-        assert required_params.issubset(kwargs.keys()), f"Missing arguments: {required_params.difference(kwargs.keys())}"
-
+            eval_results = self.evaluate_method(method)
+            scores.update(eval_results)
+    
+    def evaluate_method(self, method: Callable):
         # Dynamically set the arguments and run the evaluation methods
-        for method in self.methods:
-            method_args = {param: kwargs[param] for param in inspect.signature(method).parameters}
-            print(f"Running evaluation method: {method.__name__}")
-            self.results[method.__name__] = method(**method_args)
+        required_params = inspect.signature(method).parameters
+        method_args = {param: self.kwargs[param] for param in required_params}
+        return method(**method_args)
+
+    def get_results(self): return self.results
+    def to_latex(self): return self.results.to_latex(index=False)
+    def save_results(self, output_path): self.results.to_csv(output_path)
+
+    def check_params_is_complete(self): 
+        # Get all params of all methods without syndata and model
+        all_required_params = set([param for method in self.methods for param in inspect.signature(method).parameters]).difference(["syndata", "model"])
+        assert all_required_params.issubset(self.kwargs.keys()), f"Missing arguments: {all_required_params.difference(self.kwargs.keys())}"
 
 
 if __name__ == "__main__":
-    from src.eval.methods import privacy_methods
-    from src.data.random_data import generate_random_data
+    parser = argparse.ArgumentParser()
 
-    train_data = generate_random_data(20,5,20)
-    test_data = generate_random_data(20,5,20)
-    threshold = 0.8
-
-    from src.models.rgan import RGAN
-    from src.training.hyperparameters import get_default_params
-    params = get_default_params("rgan", train_data.size())
-    model = RGAN(**params)
-
-    eval = Evaluator(privacy_methods)
+    # Privacy arguments
+    parser.add_argument('--k', type=int, help="Number of neighbors in knn", default=1)
+    parser.add_argument('--aia_threshold', type=float, help="", default=0.8)
+    parser.add_argument('--mia_threshold', type=float, help="", default=0.8)
+    parser.add_argument('--reid_threshold', type=float, help="", default=0.8)
+    parser.add_argument('--matching_threshold', type=float, help="", default=0.8)
+    parser.add_argument('--num_disclosed_attributes', type=int, help="Number of disclosed attributes in attribute inference attack", default=3)
     
-    args = {
-        "train_data": train_data,
-        "test_data": test_data,
-        "threshold": threshold,
-        "model": model
-    }
+    # Diversity arguments
+    parser.add_argument('--n_components', type=int, help="Number of componenents in pca", default=10)
+    parser.add_argument('--n_neighbors', type=int, help="Number of neighbors in knn", default=5)
+    parser.add_argument('--reshape_method', type=str, help="How to reshape the data?", choices=['sequences', 'events'], default=0)
 
-    eval.evaluate(**args)
-    print(eval.results)
+    method_args = vars(parser.parse_args())
+
+    method_args.update({
+            "train_data": None,
+            "test_data": None,
+            "columns": None,
+            "model": None
+    })
+
+    eval = Evaluator(diversity_methods, **method_args)
+    eval.evaluate(f"outputs/syndata/1")
