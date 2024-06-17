c8c56ccb1beeddec015c02d62a717304cb384ba4
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-17 23:48:52 +0200

    Added Tuner class

--- src/training/tuning.py
+++ src/training/tuning.py
@@ -8,59 +8,48 @@ from src.training.hyperparameters import get_grid, add_shape_to_params
 from src.utilities.utils import create_directory, save_trial
 from src.models.models import train_model
 
-from datetime import datetime
+class Tuner:
+    def __init__(self, dataset, name_data: str, seed: int, exp_dir: str):
+        self.name_data = name_data
+        self.dataset = dataset
+        self.seed = seed
 
-def objective(model_class, trial, dataset: Dataset, path:str, n_folds: int, epochs: int, moment):
-    
-    # Set hyperparameter grid
-    hyperparams = get_grid(model_class, trial, dataset.sequences.shape)
+        self.parameter_folder = exp_dir + "hyperparams/"
 
-    kfold = KFold(n_splits=n_folds, shuffle=True, random_state=42)
-    val_losses = []
+    def tune(self, model, trials, folds, epochs):
+        suffix = f"{self.name_data}-{model.NAME}-{self.seed}"
+        print(f"sqlite:///{self.parameter_folder}trials/{suffix}.db")
+        storage = optuna.storages.RDBStorage(url=f'sqlite:///{self.parameter_folder}trials/{suffix}.db')
 
-    for fold, (train_idx, val_idx) in enumerate(kfold.split(dataset)):
-        print(f"Starting trial {trial.number}, fold {fold}")
-        train_data = Subset(dataset, train_idx)
-        val_data = Subset(dataset, val_idx)
-        
-        model = model_class(**hyperparams)
-
-        # Set paths and create directories
-        fold_log_dir = f"{path}/runs/{dataset.NAME}-{model.NAME}/{moment}/trial_{trial.number}_fold_{fold}/"
-        loss_log_dir = f"{path}/losses/{dataset.NAME}-{model.NAME}/{moment}/trial_{trial.number}_fold_{fold}/"
-        create_directory(fold_log_dir)
-        create_directory(loss_log_dir)
-
-        # Train classifier
-        fold_val_loss = train_model(model, train_data, epochs, val_data, fold_log_dir, loss_log_dir)
-
-        val_losses.append(fold_val_loss)
+        study = optuna.create_study(
+            study_name=suffix,  # Use the formatted suffix
+            direction="minimize",
+            storage=storage,
+            load_if_exists=True,
+            pruner=MedianPruner()
+        )
+        study.optimize(lambda trial: self.objective(trial, model, folds, epochs), n_trials=trials)
 
 
-    avg_val_loss = sum(val_losses) / len(val_losses)
-    return avg_val_loss
+    def objective(self, trial, model_class, folds, epochs):
+        self.shape = (len(self.dataset), *self.dataset[0].shape)
+        hyperparams = get_grid(model_class, trial, self.shape)
+        val_losses = []
 
+        # Cross-validation
+        kfold = KFold(n_splits=folds, shuffle=True, random_state=42)
+        for fold, (train_idx, val_idx) in enumerate(kfold.split(self.dataset)):
+            print(f"Starting trial {trial.number}, fold {fold}")
 
-def optimize_hyperparameters(dataset, model, output_path: str, epochs: int, n_trials=10, n_folds=5):
-    moment = datetime.now().strftime('%Y-%m-%d_%H-%M')
-    storage = optuna.storages.RDBStorage(url=f'sqlite:///outputs/hyperparams/trials/{dataset.NAME}-{model.NAME}.db')
+            # Store data in datasets
+            train_data = Subset(self.dataset, train_idx)
+            val_data = Subset(self.dataset, val_idx)
 
-    study = optuna.create_study(study_name=f"{dataset.NAME}-{model.NAME}", direction="minimize", storage=storage, load_if_exists=True, pruner=MedianPruner)
-    study.optimize(lambda trial: objective(model, trial, dataset, output_path, n_folds, epochs, moment), n_trials=n_trials)
+            # Load and train model
+            model = model_class(**hyperparams)
+            fold_val_loss = train_model(model, train_data, epochs, val_data)
+            val_losses.append(fold_val_loss)
 
-    return study.best_trial
-
-def save_best_trial(old_trial, new_trial, path:str):
-    # If score is better, than overwrite saved params
-    if old_trial is None:
-        print("Model has not been tuned before, saving hyperparameter..")
-        save_trial(new_trial, path)
-    else:
-        print(f"Old score: {old_trial["value"]}")
-        print(f"New score: {new_trial.value}")
-
-        if new_trial.value < old_trial["value"]:
-            save_trial(new_trial, path)
-
-        print(f"Best hyperparameters: {new_trial.params}")
+        avg_val_loss = sum(val_losses) / len(val_losses)
+        return avg_val_loss
 
