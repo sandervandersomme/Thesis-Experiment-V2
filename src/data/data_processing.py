524ee343b161413372352b91fd61e9472c8dc2cc
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-17 18:44:59 +0200

    Updated data splitting script

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset, random_split
+from torch.utils.data import Dataset, random_split, Subset
 
 def flatten_into_sequences(data: torch.Tensor):
     """
@@ -19,9 +19,14 @@ def flatten_into_events(data: torch.Tensor):
 def generate_random_data(n_sequence, n_events, n_features):
     return torch.rand((n_sequence, n_events, n_features))
 
-def split_train_val_test(data: Dataset, train_split: float, val_split: float):
-    train_size = int(len(data) * train_split)
-    val_size = int(len(data) * val_split)
-    test_size = len(data) - train_size - val_size
-    lengths = [train_size, val_size, test_size]
-    return random_split(data, lengths)
+def split_train_test(dataset: Dataset, train_split: float):
+    """Returns train and test sequences"""
+
+    train_size = int(len(dataset) * train_split)
+    test_size = len(dataset) - train_size
+    lengths = [train_size, test_size]
+    train_subset, test_subset = random_split(dataset, lengths)
+
+    train_sequences = dataset.sequences[train_subset.indices]
+    test_sequences = dataset.sequences[test_subset.indices]
+    return train_sequences, test_sequences
