98b2a77ce0bb9c56f75ab0d3984cbf1235ccbcf1
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-17 13:57:12 +0200

    Added function to split datasets into train test and val

--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -1,5 +1,5 @@
 import torch
-import numpy as np
+from torch.utils.data import Dataset, random_split
 
 def flatten_into_sequences(data: torch.Tensor):
     """
@@ -18,3 +18,10 @@ def flatten_into_events(data: torch.Tensor):
 
 def generate_random_data(n_sequence, n_events, n_features):
     return torch.rand((n_sequence, n_events, n_features))
+
+def split_train_val_test(data: Dataset, train_split: float, val_split: float):
+    train_size = int(len(data) * train_split)
+    val_size = int(len(data) * val_split)
+    test_size = len(data) - train_size - val_size
+    lengths = [train_size, val_size, test_size]
+    return random_split(data, lengths)
