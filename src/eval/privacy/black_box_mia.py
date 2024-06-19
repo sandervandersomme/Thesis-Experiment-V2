a1ea98da28747a2a56f110d04567e06f603f1d57
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-19 03:34:18 +0200

    Removed MIA attack superclass

--- src/eval/privacy/black_box_mia.py
+++ src/eval/privacy/black_box_mia.py
@@ -1,21 +1,61 @@
 import torch
-from src.eval.privacy.mia_attack import MIA
-from src.models.models import GenModel, RWGAN
+from src.models.models import GenModel, RWGAN, TimeGAN
 from src.models.models import train_gen_model
 
-class BlackBoxMia(MIA):
-    def __init__(self, target_model: GenModel, syndata: torch.Tensor, epochs=50):
-        super().__init__(target_model)
-        shadow_model = target_model.__class__(**target_model.hyperparams)
-        train_gen_model(shadow_model, syndata, epochs)
+class BlackBoxMia():
+    def __init__(self, target_model: GenModel, syndata: torch.Tensor, epochs: int=50):
+        self.device = target_model.device
+        self.shadow_model = target_model.__class__(**target_model.hyperparams)
+        train_gen_model(self.shadow_model, syndata, epochs)
         
-        if isinstance(shadow_model, RWGAN):
-            self.discriminator = shadow_model.critic
+        if isinstance(self.shadow_model, RWGAN):
+            self.discriminator = self.shadow_model.critic
         else:
-            self.discriminator = shadow_model.discriminator
+            self.discriminator = self.shadow_model.discriminator
 
-def mia_blackbox_attack(train_data, test_data, model:GenModel, mia_threshold, epochs):
-    syndata = model.generate_data(len(train_data))
+    def attack(self, train_data: torch.tensor, test_data: torch.tensor, threshold: float):
+        # Configure attack data
+        mixed_data = torch.cat((train_data, test_data), dim=0).to(self.device)
+        train_labels = torch.ones(train_data.size(0)).to(self.device)
+        test_labels = torch.zeros(test_data.size(0)).to(self.device)
+        labels = torch.cat((train_labels, test_labels), dim=0).to(self.device)
+
+        # Start attack
+        self.discriminator.eval()
+
+        # Get discriminator confidence scores
+        with torch.no_grad():
+            if isinstance(self.shadow_model, TimeGAN):
+                predictions = attack_timegan(self.shadow_model, mixed_data)
+            else:
+                predictions = self.discriminator(mixed_data)
+        
+        scores_vs_labels = list(zip(predictions, labels.tolist()))
+        scores_vs_labels.sort(key=lambda x: x[0], reverse=True)
+
+        train_scores = [(score.item(), label) for (score, label) in scores_vs_labels if label == 1]
+
+        # Calculate risk
+        high_confidence_scores = [score for (score, _) in train_scores if score >= threshold]
+        num_samples_at_risk = len(high_confidence_scores)
+
+        # Calculate attacker accuracy
+        correct_predictions = sum(1 for (score, label) in scores_vs_labels if int(score) == label)
+        total_predictions = len(scores_vs_labels)
+        accuracy = correct_predictions / total_predictions
+
+        return {
+            "BBMIA samples at risk": num_samples_at_risk,
+            "BBMIA samples at risk": num_samples_at_risk/len(train_data),
+            "BBMIA total accuracy": accuracy
+        }
+
+def attack_timegan(model: TimeGAN, data):
+    outputs = model.embedder(data)
+    outputs = model.discriminator(outputs)
+    return outputs
+
+def mia_blackbox_attack(syndata: torch.Tensor, train_data: torch.Tensor, test_data: torch.Tensor, model:GenModel, mia_threshold: float, epochs: int):
     mia = BlackBoxMia(model, syndata, epochs)
     results = mia.attack(train_data, test_data, mia_threshold)
     return results
--- src/eval/privacy/white_box_mia.py
+++ src/eval/privacy/white_box_mia.py
@@ -1,17 +1,58 @@
+from src.models.models import RWGAN
+from src.models.models import GenModel, TimeGAN
 import torch
 
-from src.models.models import RWGAN
-from src.eval.privacy.mia_attack import MIA
-from src.models.models import GenModel
-
-class WhiteBoxMIA(MIA):
+class WhiteBoxMIA():
     def __init__(self, model: GenModel):
-        super().__init__(model)
+        self.device = model.device
+        self.model = model
 
         if isinstance(model, RWGAN):
             self.discriminator = model.critic
         else:
             self.discriminator = model.discriminator
+
+    def attack(self, train_data: torch.tensor, test_data: torch.tensor, threshold: float):
+        # Configure attack data
+        mixed_data = torch.cat((train_data, test_data), dim=0).to(self.device)
+        train_labels = torch.ones(train_data.size(0)).to(self.device)
+        test_labels = torch.zeros(test_data.size(0)).to(self.device)
+        labels = torch.cat((train_labels, test_labels), dim=0).to(self.device)
+
+        # Start attack
+        self.discriminator.eval()
+
+        # Get discriminator confidence scores
+        with torch.no_grad():
+            if isinstance(self.model, TimeGAN):
+                predictions = attack_timegan(self.model, mixed_data)
+            else:
+                predictions = self.discriminator(mixed_data)
+        
+        scores_vs_labels = list(zip(predictions, labels.tolist()))
+        scores_vs_labels.sort(key=lambda x: x[0], reverse=True)
+
+        train_scores = [(score.item(), label) for (score, label) in scores_vs_labels if label == 1]
+
+        # Calculate risk
+        high_confidence_scores = [score for (score, _) in train_scores if score >= threshold]
+        num_samples_at_risk = len(high_confidence_scores)
+
+        # Calculate attacker accuracy
+        correct_predictions = sum(1 for (score, label) in scores_vs_labels if int(score) == label)
+        total_predictions = len(scores_vs_labels)
+        accuracy = correct_predictions / total_predictions
+
+        return {
+            "WBMIA samples at risk": num_samples_at_risk,
+            "WBMIA samples at risk": num_samples_at_risk/len(train_data),
+            "WBMIA total accuracy": accuracy
+        }
+
+def attack_timegan(model: TimeGAN, data):
+    outputs = model.embedder(data)
+    outputs = model.discriminator(outputs)
+    return outputs
 
 def mia_whitebox_attack(train_data, test_data, model, mia_threshold):
     mia = WhiteBoxMIA(model)
