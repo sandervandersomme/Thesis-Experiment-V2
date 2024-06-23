547d6e2b448de0add7d1b3e0bd107a6a091f0ccd
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-23 19:11:29 +0200

    Updated evaluator

--- src/eval/evaluators/evaluator.py
+++ src/eval/evaluators/evaluator.py
@@ -1,12 +1,13 @@
 from typing import List
 from src.utils import load_dataset
 import os
+import pandas as pd
 
 class Evaluator():
-    def __init__(self, eval_args: str, eval_dir) -> None:
+    def __init__(self, eval_args: str, output_dir) -> None:
         self.eval_args = eval_args
-        self.eval_dir = os.path.join(eval_dir, "eval/")
-        self.syn_data_dir = os.path.join(eval_dir, "syndata/")
+        self.eval_dir = os.path.join(output_dir, "eval/")
+        self.syn_data_dir = os.path.join(output_dir, "syndata/")
 
         # Collect results
         self.results = None
@@ -15,9 +16,10 @@ class Evaluator():
     def evaluate(self, files: List[str]):
         for id, filename in enumerate(files):
             syn_dataset = load_dataset(self.syn_data_dir, filename)
-            self._evaluate_dataset(syn_dataset, id)
+            self._evaluate_dataset(syn_dataset)
         
-        self._post_processing()
+        avgs_scores, full_scores = self._post_processing()
+        return avgs_scores, full_scores
 
     def _evaluate_dataset(self):
         raise NotImplementedError
