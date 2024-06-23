0e22cbc9ae1d15cb99c2edfaed88b9d22eaa5c08
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-23 19:11:03 +0200

    Updated evaluators with basic structure

--- src/eval/evaluators/fidelity_evaluator.py
+++ src/eval/evaluators/fidelity_evaluator.py
@@ -5,11 +5,13 @@ from typing import List
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 class FidelityEvaluator(Evaluator):
     def __init__(self, eval_args, output_dir: str) -> None:
         super().__init__(eval_args, output_dir)
          
+        self.eval_dir = os.path.join(self.eval_dir, "fidelity/")
         self.all_statistics = []
         self.all_correlations = []
         self.all_distances = []
@@ -17,7 +19,7 @@ class FidelityEvaluator(Evaluator):
     def evaluate(self, files: List[str]):
         return super().evaluate(files)
 
-    def _evaluate_dataset(self, syndata: torch.Tensor, dataset_id: int):
+    def _evaluate_dataset(self, syndata: torch.Tensor):
         results_statistics = similarity_of_statistics(self.eval_args.real_data.sequences, syndata, self.eval_args.columns)
         results_correlations = similarity_of_correlations(self.eval_args.real_data.sequences, syndata)
         results_distances = wasserstein_distance(self.eval_args.real_data.sequences, syndata, self.eval_args.columns)
@@ -32,6 +34,11 @@ class FidelityEvaluator(Evaluator):
         unzipped_correlations = zip(*self.all_correlations)
         correlation_scores, correlation_matrices = map(list, unzipped_correlations)
 
+        # Save non-averaged results to dataframe
+        non_avg_scores_df = pd.DataFrame(statistics)
+        non_avg_scores_df['Correlation Score'] = pd.Series(correlation_scores)
+        non_avg_scores_df['Distance Score'] = pd.Series(self.all_distances)
+
         # Calculate averages
         avg_statistic_scores = sum(statistics)/len(statistics)
         avg_statistics_per_variable = sum(statistics_per_variable)/len(statistics_per_variable)
@@ -39,8 +46,8 @@ class FidelityEvaluator(Evaluator):
         avg_correlation_matrix = sum(correlation_matrices)/len(correlation_matrices)
         avg_distance = sum(self.all_distances)/len(self.all_distances)
 
-        # Save to dataframe
-        avg_results = pd.DataFrame({
+        # Save averaged results to dataframe
+        avg_scores_df = pd.DataFrame({
             'Statistic Mean': [avg_statistic_scores.iloc[0]],
             'Statistic Std': [avg_statistic_scores.iloc[1]],
             'Statistic Median': [avg_statistic_scores.iloc[2]],
@@ -48,18 +55,26 @@ class FidelityEvaluator(Evaluator):
             'Correlation Score': [avg_correlation_score],
             'Distance Score': [avg_distance]
         })
-        print(avg_results)
+        
+        # Create DataFrame for avg_statistics_per_variable
+        avg_statistics_df = pd.DataFrame(avg_statistics_per_variable)
 
-        # # Visualize correlation matrix
-        # plt.figure(figsize=(10, 8))
-        # sns.heatmap(avg_correlation_matrix, annot=True, cmap='coolwarm')
-        # plt.title('Average Correlation Matrix')
-        # plt.show()
+        # Convert avg_statistics_per_variable table to LaTeX and save to file
+        latex_table = avg_statistics_df.to_latex(index=False)
+        with open(f'{self.eval_dir}/{self.eval_args.model_type}_non_avg_statistics.tex', 'w') as f:
+            f.write(latex_table)
 
-        # # Print table of statistics
-        # print(df)
+        # Convert avg_statistics_per_variable table to Markdown and save to file
+        markdown_table = avg_statistics_df.to_markdown(index=False)
+        with open(f'{self.eval_dir}/{self.eval_args.model_type}_non_avg_statistics.md', 'w') as f:
+            f.write(markdown_table)
 
-        # # Save dataframe to CSV
-        # df.to_csv(f'{self.output_dir}/evaluation_results.csv', index=False)
+        # Visualize correlation matrix
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(avg_correlation_matrix, annot=True, cmap='coolwarm')
+        plt.title('Average Correlation Matrix')
+        plot_path = f'{self.eval_dir}/{self.eval_args.model_type}_heatmap_correlations.png'
+        plt.savefig(plot_path)
+        plt.close()
 
-        # return df
+        return avg_scores_df, non_avg_scores_df
--- src/eval/evaluators/utility_evaluator.py
+++ src/eval/evaluators/utility_evaluator.py
@@ -1,9 +1,18 @@
 from src.eval.evaluators.evaluator import Evaluator
-
+from typing import List
+import torch
+import os
 
 class UtilityEvaluator(Evaluator):
-    def __init__(self, num_datasets: int, num_instances: int, eval_dir: str) -> None:
-        super().__init__(num_datasets, num_instances, eval_dir)
+    def __init__(self, eval_args, output_dir: str) -> None:
+        super().__init__(eval_args, output_dir)         
+        self.eval_dir = os.path.join(self.eval_dir, "utility/")
 
-    def evaluate_dataset(self, model_type: str, args):
+    def evaluate(self, files: List[str]):
+        return super().evaluate(files)
+
+    def _evaluate_dataset(self, syndata: torch.Tensor):
+        raise NotImplementedError
+
+    def _post_processing(self):
         raise NotImplementedError
