54e8eee72eb3c4a28d90dc61a473e94347f32e18
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-25 01:47:18 +0200

    edited outputs of downstream task scripts

--- src/eval/utility/classification.py
+++ src/eval/utility/classification.py
@@ -8,42 +8,52 @@ import numpy as np
 from src.data.data_processing import split
 
 def classification_scores(real_train_data: DownstreamDataset, syndata: DownstreamDataset, test_data: DownstreamDataset, epochs: int, hyperparams: dict, val_split_size: float, seed):
+    test_labels, testcounts = torch.unique(test_data.targets, return_counts=True)
+    train_labels, traincounts = torch.unique(real_train_data.targets, return_counts=True)
+    syn_labels, syncounts = torch.unique(syndata.targets, return_counts=True)
+
     train_data, val_data = split(real_train_data, val_split_size, seed)
     
-    predictions_on_real = run_classifier(train_data, val_data,test_data, epochs, hyperparams, val_split_size)
+    print("Training classification model on real data")
+    predictions_on_real = run_classifier(train_data, val_data, test_data, epochs, hyperparams, val_split_size)
+    print("Training classification model on synthetic data")
     predictions_on_syn = run_classifier(syndata, val_data, test_data, epochs, hyperparams, val_split_size)
     true_labels = test_data.targets
 
-    return {
-        'Accuracy Real': accuracy_score(true_labels, predictions_on_real),
-        'Accuracy Synthetic': accuracy_score(true_labels, predictions_on_syn),
-        'Precision Real': precision_score(true_labels, predictions_on_real),
-        'Precision Synthetic': precision_score(true_labels, predictions_on_syn),
-        'Recall Real': recall_score(true_labels, predictions_on_real),
-        'Recall Synthetic': recall_score(true_labels, predictions_on_syn),
-        'F1 Score Real': f1_score(true_labels, predictions_on_real, zero_division=0),
-        'F1 Score Synthetic': f1_score(true_labels, predictions_on_syn, zero_division=0),
-        'ROC AUC Real': roc_auc_score(true_labels, predictions_on_real),
-        'ROC AUC Synthetic': roc_auc_score(true_labels, predictions_on_syn),
-        'Confusion Matrix Real': confusion_matrix(true_labels, predictions_on_real),
-        'Diff Accuracy': np.abs(accuracy_score(true_labels, predictions_on_real) - accuracy_score(true_labels, predictions_on_syn)),
-        'Diff Precision': np.abs(precision_score(true_labels, predictions_on_real) - precision_score(true_labels, predictions_on_syn)),
-        'Diff Recall': np.abs(recall_score(true_labels, predictions_on_real) - recall_score(true_labels, predictions_on_syn)),
-        'Diff F1 Score': f1_score(true_labels, predictions_on_real, zero_division=0) - f1_score(true_labels, predictions_on_syn, zero_division=0),
-        'Diff ROC AUC': np.abs(roc_auc_score(true_labels, predictions_on_real) - roc_auc_score(true_labels, predictions_on_syn)),
-        'Confusion Matrix Synthetic': confusion_matrix(true_labels, predictions_on_syn),
-        'Confusion Matrix Real': confusion_matrix(true_labels, predictions_on_real)
+    raw_results = {
+        "train_counts": (train_labels, testcounts),
+        "test_counts": (test_labels, testcounts),
+        "syn_counts": (syn_labels, syncounts),
+        "accuracy_real": accuracy_score(true_labels, predictions_on_real),
+        "accuracy_synthetic": accuracy_score(true_labels, predictions_on_syn),
+        "precision_real": precision_score(true_labels, predictions_on_real, zero_division=0),
+        "precision_synthetic": precision_score(true_labels, predictions_on_syn, zero_division=0),
+        "recall_real": recall_score(true_labels, predictions_on_real),
+        "recall_synthetic": recall_score(true_labels, predictions_on_syn),
+        "f1_score_real": f1_score(true_labels, predictions_on_real, zero_division=0),
+        "f1_score_synthetic": f1_score(true_labels, predictions_on_syn, zero_division=0),
+        "roc_auc_real": roc_auc_score(true_labels, predictions_on_real),
+        "roc_auc_synthetic": roc_auc_score(true_labels, predictions_on_syn),
+        "confusion_matrix_synthetic": confusion_matrix(true_labels, predictions_on_syn),
+        "confusion_matrix_real": confusion_matrix(true_labels, predictions_on_real)
     }
-    # diffs = np.abs(metrics["Synthetic"] - metrics["Real"])
-    # Test for statistical significance
-    # implement bar charts for accuracy, precision, recall and f1-scores
-    # Print confusion matrices
+    
+    scores = {
+        "diff_accuracy": accuracy_score(true_labels, predictions_on_syn) - accuracy_score(true_labels, predictions_on_real),
+        "diff_precision": precision_score(true_labels, predictions_on_syn, zero_division=0) - precision_score(true_labels, predictions_on_real, zero_division=0),
+        "diff_recall": recall_score(true_labels, predictions_on_syn) - recall_score(true_labels, predictions_on_real),
+        "diff_f1_score": f1_score(true_labels, predictions_on_syn, zero_division=0) - f1_score(true_labels, predictions_on_real, zero_division=0),
+        "diff_roc_auc": roc_auc_score(true_labels, predictions_on_syn) - roc_auc_score(true_labels, predictions_on_real),
+        "diff_matrix": confusion_matrix(true_labels, predictions_on_syn) - confusion_matrix(true_labels, predictions_on_real)
+    }
+
+    return scores, raw_results
 
 def run_classifier(train_data: Dataset, val_data: Dataset, test_data: Dataset, epochs: int, hyperparams: dict, val_split_size: float):
     # split real data into train and validation
 
     classifier = TimeseriesClassifier(**hyperparams)
-    train_classifier(classifier, train_data, val_data, epochs)
+    train_classifier(classifier, train_data, val_data, epochs, verbose=False)
 
     classifier.eval()
     with torch.no_grad():
--- src/eval/utility/regression.py
+++ src/eval/utility/regression.py
@@ -10,33 +10,41 @@ from src.data.data_processing import split
 def regression_scores(real_train_data: DownstreamDataset, syndata: DownstreamDataset, test_data: DownstreamDataset, epochs: int, hyperparams: dict, val_split_size: float, seed):
     train_data, val_data = split(real_train_data, val_split_size, seed)
     
+    print("Training regression model on real data")
     predictions_on_real = run_regressor(train_data, val_data, test_data, epochs, hyperparams)
+    print("Training regression model on synthetic data")
     predictions_on_syn = run_regressor(syndata, val_data, test_data, epochs, hyperparams)
     true_labels = test_data.targets.numpy()
 
     predictions_on_real = predictions_on_real.numpy()  # Ensure predictions are NumPy arrays
     predictions_on_syn = predictions_on_syn.numpy()
 
-    return {
-        'MAE Real': mean_absolute_error(true_labels, predictions_on_real),
-        'MAE Synthetic': mean_absolute_error(true_labels, predictions_on_syn),
-        'MSE Real': mean_squared_error(true_labels, predictions_on_real),
-        'MSE Synthetic': mean_squared_error(true_labels, predictions_on_syn),
-        'RMSE Real': np.sqrt(mean_squared_error(true_labels, predictions_on_real)),
-        'RMSE Synthetic': np.sqrt(mean_squared_error(true_labels, predictions_on_syn)),
-        'R2 Real': r2_score(true_labels, predictions_on_real),
-        'R2 Synthetic': r2_score(true_labels, predictions_on_syn),
-        'Diff MAE': np.abs(mean_absolute_error(true_labels, predictions_on_real) - mean_absolute_error(true_labels, predictions_on_syn)),
-        'Diff MSE': np.abs(mean_squared_error(true_labels, predictions_on_real) - mean_squared_error(true_labels, predictions_on_syn)),
-        'Diff RMSE': np.abs(np.sqrt(mean_squared_error(true_labels, predictions_on_real)) - np.sqrt(mean_squared_error(true_labels, predictions_on_syn))),
-        'Diff R2': np.abs(r2_score(true_labels, predictions_on_real) - r2_score(true_labels, predictions_on_syn))
+    raw_results = {
+        'MAE_Real': mean_absolute_error(true_labels, predictions_on_real),
+        'MAE_Synthetic': mean_absolute_error(true_labels, predictions_on_syn),
+        'MSE_Real': mean_squared_error(true_labels, predictions_on_real),
+        'MSE_Synthetic': mean_squared_error(true_labels, predictions_on_syn),
+        'RMSE_Real': np.sqrt(mean_squared_error(true_labels, predictions_on_real)),
+        'RMSE_Synthetic': np.sqrt(mean_squared_error(true_labels, predictions_on_syn)),
+        'R2_Real': r2_score(true_labels, predictions_on_real),
+        'R2_Synthetic': r2_score(true_labels, predictions_on_syn),
     }
+    
+    scores = {
+        'diff_MAE': np.abs(mean_absolute_error(true_labels, predictions_on_syn) - mean_absolute_error(true_labels, predictions_on_real)),
+        'diff_MSE': np.abs(mean_squared_error(true_labels, predictions_on_syn) - mean_squared_error(true_labels, predictions_on_real)),
+        'diff_RMSE': np.abs(np.sqrt(mean_squared_error(true_labels, predictions_on_syn)) - np.sqrt(mean_squared_error(true_labels, predictions_on_real))),
+        'diff_R2': np.abs(r2_score(true_labels, predictions_on_syn) - r2_score(true_labels, predictions_on_real))
+    }
+
+    return scores, raw_results
+
 
 def run_regressor(train_data: Dataset, val_data: Dataset, test_data: Dataset, epochs: int, hyperparams: dict):
     # split real data into train and validation
 
     regressor = TimeseriesRegressor(**hyperparams)
-    train_regressor(regressor, train_data, val_data, epochs)
+    train_regressor(regressor, train_data, val_data, epochs, verbose=False)
 
     regressor.eval()
     with torch.no_grad():
