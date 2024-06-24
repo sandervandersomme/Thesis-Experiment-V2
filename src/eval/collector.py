7bc8016b03070e54f648084e85bf92c6d9d6520e
Sander van der Somme - UMCU laptop <s.vandersomme@outlook.com>
2024-06-24 14:16:00 +0200

    Updated collector

--- src/eval/collector.py
+++ src/eval/collector.py
@@ -20,6 +20,7 @@ class Collector():
         self.num_datasets = num_datasets
 
         self.output_dir = output_dir
+        self.eval_dir = os.path.join(output_dir, 'eval/')
         self.args = args
 
         self.results_full = pd.DataFrame()
@@ -32,6 +33,7 @@ class Collector():
 
             self.collect_model_results(model_type, model_files)
         # Save results to files
+
         self.save_results()
 
     def collect_model_results(self, model: str, model_files: List[str]):
@@ -42,35 +44,41 @@ class Collector():
         # Get files of datasets
         filenames_datasets = get_filenames_of_syndatasets(model, self.num_instances, self.num_datasets)
         
+        model_scores_df = pd.DataFrame()
+        avg_model_scores_df = pd.DataFrame()
+
         # Use evaluators for evaluation of the datasets
         for evaluator in self.evaluators:
-            avgs_scores, full_scores = evaluator.evaluate(filenames_datasets)
-            
-            # Add average scores to the results_average dataframe
-            avgs_scores['Model'] = model  # Add model information as a column
-            self.results_average = pd.concat([self.results_average, avgs_scores], axis=0)
+            new_avg_model_scores, new_full_scores = evaluator.evaluate(filenames_datasets)
 
-            # Add full scores to the results_full dataframe
-            full_scores_df = pd.DataFrame(full_scores)
-            full_scores_df['Model'] = model
-            self.results_full = pd.concat([self.results_full, full_scores_df], axis=0)
+            model_scores_df = pd.concat([model_scores_df, new_full_scores], axis=1)
+            avg_model_scores_df = pd.concat([avg_model_scores_df, new_avg_model_scores], axis=1)
+                
+        
+
+        model_scores_df['Model'] = model
+        avg_model_scores_df['Model'] = model  # Add model information as a column
+        self.results_average = pd.concat([self.results_average, avg_model_scores_df], axis=0)
+        self.results_full = pd.concat([self.results_full, model_scores_df], axis=0)
+
+
         
     def save_results(self):
         # Ensure the output directory exists
         os.makedirs(self.output_dir, exist_ok=True)
         
         # Save the average results
-        avg_results_path_csv = os.path.join(self.output_dir, 'average_results.csv')
-        avg_results_path_latex = os.path.join(self.output_dir, 'average_results.tex')
-        avg_results_path_md = os.path.join(self.output_dir, 'average_results.md')
+        avg_results_path_csv = os.path.join(self.eval_dir, 'average_results.csv')
+        avg_results_path_latex = os.path.join(self.eval_dir, 'average_results.tex')
+        avg_results_path_md = os.path.join(self.eval_dir, 'average_results.md')
         self.results_average.to_csv(avg_results_path_csv, index=False)
         self.results_average.to_latex(avg_results_path_latex, index=False)
         self.save_markdown(self.results_average, avg_results_path_md)
         
         # Save the full results
-        full_results_path_csv = os.path.join(self.output_dir, 'full_results.csv')
-        full_results_path_latex = os.path.join(self.output_dir, 'full_results.tex')
-        full_results_path_md = os.path.join(self.output_dir, 'full_results.md')
+        full_results_path_csv = os.path.join(self.eval_dir, 'full_results.csv')
+        full_results_path_latex = os.path.join(self.eval_dir, 'full_results.tex')
+        full_results_path_md = os.path.join(self.eval_dir, 'full_results.md')
         self.results_full.to_csv(full_results_path_csv, index=False)
         self.results_full.to_latex(full_results_path_latex, index=False)
         self.save_markdown(self.results_full, full_results_path_md)
@@ -84,19 +92,21 @@ class Collector():
         return markdown
 
 def create_evaluators(criteria: List[str], args, output_dir) -> List[Evaluator]:
-    if criteria == "all":
+    if "all" in criteria:
         return [
-            FidelityEvaluator(args, output_dir)
+            FidelityEvaluator(args, output_dir),
+            TemporalFidelityEvaluator(args, output_dir),
+            DiversityEvaluator(args, output_dir)
         ]
     else: 
+        evaluators = []
         for criterion in criteria:
-            evaluators = []
             if criterion == "fidelity":
                 evaluators.append(FidelityEvaluator(args, output_dir))
             if criterion == "temporal":
                 evaluators.append(TemporalFidelityEvaluator(args, output_dir))
             if criterion == "diversity":
-                raise NotImplementedError
+                evaluators.append(DiversityEvaluator(args, output_dir))
             if criterion == "utility":
                 raise NotImplementedError
             if criterion == "privacy":
