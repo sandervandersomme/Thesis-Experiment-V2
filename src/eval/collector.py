8916f6b71b7c06484567d5df90a8e02afe583525
sandervandersomme <57229133+sandervandersomme@users.noreply.github.com>
2024-06-26 14:43:28 +0200

    Reimplemented collector

--- src/eval/collector.py
+++ src/eval/collector.py
@@ -1,6 +1,6 @@
 from typing import List
 import os
-from src.utils import get_filenames_of_models, get_filenames_of_syndatasets, load_model
+from src.utils import load_model, load_syndata, save_df_to_csv
 
 # Import evaluators
 from src.eval.evaluators.evaluator import Evaluator
@@ -22,97 +22,54 @@ class Collector():
         self.output_dir = output_dir
         self.eval_dir = os.path.join(output_dir, 'eval/')
         self.model_dir = os.path.join(output_dir, 'models/')
+        self.syndata_dir = os.path.join(output_dir, 'syndata/')
         self.args = args
 
         self.results_full = pd.DataFrame()
         # self.results_average = pd.DataFrame()
 
+        self.setup_folders()
+
+    def setup_folders(self):
+        for criterion in self.criteria:
+            path = os.path.join(self.eval_dir, f"{criterion}/")
+            os.makedirs(path, exist_ok=True)
+
     def collect_results(self):
         for model_type in self.args.models:
             self.args.model_type = model_type
-            model_files = get_filenames_of_models(model_type, self.num_instances)
 
-            self.collect_model_results(model_type, model_files)
-
-        self.results_average = self.results_full.groupby('Model').mean().reset_index()
-        self.save_results()
-
-
-    def collect_model_results(self, model: str, model_files: List[str]):
-        print(f"Start evaluating model {model}..")
-
-        # Create new evaluators for model
-        self.evaluators = create_evaluators(self.criteria, self.args, self.output_dir)
-
-        # Get files of datasets
-        filenames_datasets = get_filenames_of_syndatasets(model, self.num_instances, self.num_datasets)
+            for model_id in range(self.args.num_instances):
+                path = os.path.join(self.model_dir, f"{model_type}-{model_id}.pkl")
+                self.args.model = load_model(path)
+                self.args.model_id = model_id
+                for syndata_id in range(self.args.num_instances):
+                    path = os.path.join(self.syndata_dir, f"{model_type}-{model_id}-{syndata_id}.pt")
+                    syndata = load_syndata(path)
+                    self.args.syndata_id = syndata_id
+                    for criterion in self.criteria:
+                        evaluator = select_evaluator(criterion, self.args, self.output_dir)
+                        evaluator.syndata = syndata
+                        evaluator.evaluate()
         
-        # Initialise dataframe for collecting model results
-        model_scores_df = pd.DataFrame()
+        self.combine_results()
 
-        # Use evaluators for evaluation of the datasets
-        for id in range(len(self.evaluators)):
-            evaluator = self.evaluators.pop()
-            for filename in model_files:
-                self.args.model = load_model(self.model_dir, filename)
-                new_full_scores = evaluator.evaluate(filenames_datasets)
-                model_scores_df = pd.concat([model_scores_df, new_full_scores], axis=1)
-
-        # Add model information
-        model_scores_df['Model'] = model
-
-        # Add model results to all results
-        self.results_full = pd.concat([self.results_full, model_scores_df], axis=0)
+    def combine_results(self):
+        raise NotImplementedError
 
 
-    def save_results(self):
-        # Ensure the output directory exists
-        os.makedirs(self.output_dir, exist_ok=True)
-        
-        # Save the average results
-        avg_results_path_csv = os.path.join(self.eval_dir, 'average_results.csv')
-        avg_results_path_latex = os.path.join(self.eval_dir, 'average_results.tex')
-        avg_results_path_md = os.path.join(self.eval_dir, 'average_results.md')
-        self.results_average.to_csv(avg_results_path_csv, index=False)
-        self.results_average.to_latex(avg_results_path_latex, index=False)
-        self.save_markdown(self.results_average, avg_results_path_md)
-        
-        # Save the full results
-        full_results_path_csv = os.path.join(self.eval_dir, 'full_results.csv')
-        full_results_path_latex = os.path.join(self.eval_dir, 'full_results.tex')
-        full_results_path_md = os.path.join(self.eval_dir, 'full_results.md')
-        self.results_full.to_csv(full_results_path_csv, index=False)
-        self.results_full.to_latex(full_results_path_latex, index=False)
-        self.save_markdown(self.results_full, full_results_path_md)
 
-    def save_markdown(self, df: pd.DataFrame, path: str):
-        with open(path, 'w') as f:
-            f.write(self.dataframe_to_markdown(df))
 
-    def dataframe_to_markdown(self, df: pd.DataFrame) -> str:
-        markdown = df.to_markdown(index=False)
-        return markdown
-
-def create_evaluators(criteria: List[str], args, output_dir) -> List[Evaluator]:
-    if "all" in criteria:
-        return [
-            FidelityEvaluator(args, output_dir),
-            TemporalFidelityEvaluator(args, output_dir),
-            DiversityEvaluator(args, output_dir),
-            UtilityEvaluator(args, output_dir),
-            PrivacyEvaluator(args, output_dir)
-        ]
-    else: 
-        evaluators = []
-        for criterion in criteria:
-            if criterion == "fidelity":
-                evaluators.append(FidelityEvaluator(args, output_dir))
-            if criterion == "temporal":
-                evaluators.append(TemporalFidelityEvaluator(args, output_dir))
-            if criterion == "diversity":
-                evaluators.append(DiversityEvaluator(args, output_dir))
-            if criterion == "utility":
-                evaluators.append(UtilityEvaluator(args, output_dir))
-            if criterion == "privacy":
-                evaluators.append(PrivacyEvaluator(args, output_dir))
-    return evaluators
+def select_evaluator(criterion: str, args, output_dir: str):
+    if criterion == "fidelity":
+        return FidelityEvaluator(args, output_dir)
+    if criterion == "temporal":
+        return TemporalFidelityEvaluator(args, output_dir)
+    if criterion == "diversity":
+        return DiversityEvaluator(args, output_dir)
+    if criterion == "utility":
+        return UtilityEvaluator(args, output_dir)
+    if criterion == "privacy":
+        return PrivacyEvaluator(args, output_dir)
+    
+    raise NotImplementedError
