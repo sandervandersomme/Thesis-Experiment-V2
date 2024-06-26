1560f9ca71134f2db4bf52f846c8f3343cc92997
sandervandersomme <57229133+sandervandersomme@users.noreply.github.com>
2024-06-26 02:17:34 +0200

    Updated data loader for random downstream data

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -3,10 +3,11 @@ import torch
 import numpy as np
 import pandas as pd
 
-from src.data.cf import CF
+from src.data.cf.cf import CF
 from torch.utils.data import Dataset
-from src.data.cf_classification import create_cf_classification_data
-from src.data.cf_regression import create_cf_regression_data
+from src.data.cf.cf_classification import create_cf_classification_data
+from src.data.cf.cf_regression import create_cf_regression_data
+from src.data.random_dataset.random_data import RandomDataset, RandomDownstreamClassificationDataset, RandomDownstreamRegressionDataset
 
 def load_syn_data(path): return torch.load(path)
 
@@ -14,19 +15,22 @@ def select_data(dataset: str) -> Dataset:
     "select and load dataset"
     if dataset == "cf":
         return CF()
+    if dataset == "random":
+        return RandomDataset()
 
     raise NotImplementedError
     
 def create_downstream_data(dataset: str, task: str, sequences: torch.Tensor, columns: List[str]):
-    if dataset == "cf": return cf_downstream_data(task, sequences, columns)
+    # Create CF downstream data
+    if dataset == "cf" and task == "classification": return create_cf_classification_data(sequences, columns)
+    if dataset == "cf" and task == "regression": return create_cf_regression_data(sequences, columns)
+    
+    # Create random downstream data
+    if dataset == "random" and task == "classification": return RandomDownstreamClassificationDataset()
+    if dataset == "random" and task == "regression": return RandomDownstreamRegressionDataset()
 
     raise NotImplementedError
 
-def cf_downstream_data(task, sequences, columns):
-    if task == "classification": return create_cf_classification_data(sequences, columns)
-    if task == "regression": return create_cf_regression_data(sequences, columns)
-
-    raise NotImplementedError
     
 def save_to_csv(sequences: torch.Tensor, labels: torch.Tensor, columns: List[str], filename: str):
     sequences_np = sequences.numpy()
