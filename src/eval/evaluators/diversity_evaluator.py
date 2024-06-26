69a6fcf87afaee4b794a6addb9b54615ebb90f77
sandervandersomme <57229133+sandervandersomme@users.noreply.github.com>
2024-06-26 16:12:04 +0200

    Reimplemented diversity evaluator

--- src/eval/evaluators/diversity_evaluator.py
+++ src/eval/evaluators/diversity_evaluator.py
@@ -5,66 +5,73 @@ from typing import List
 from src.eval.diversity.methods_diversity import calculate_diversity_scores
 import pandas as pd
 
+from src.utils import save_df_to_csv
+
 class DiversityEvaluator(Evaluator):
-    def __init__(self, eval_args, output_dir: str):
-        super().__init__(eval_args, output_dir)         
-        self.eval_dir = os.path.join(self.eval_dir, "diversity/")
+    def __init__(self, syndata: torch.Tensor, eval_args, output_dir: str) -> None:
+        super().__init__(syndata, eval_args, output_dir)         
 
-        # Calculate metrics for diversity of sequences
-        self.total_rel_diversity_sequences = []
-        self.total_rel_coverage_sequences = []
-        self.total_intra_real_sequences = []
-        self.total_intra_syn_sequences = []
-        self.total_inter_sequences = []
+    def setup_paths(self):
+        # Set root directory
+        self.eval_dir = os.path.join(self.eval_dir, f"diversity/{self.eval_args.model_type}")
 
-        # Calculate metrics for diversity of events
-        self.total_rel_diversity_events = []
-        self.total_rel_coverage_events = []
-        self.total_intra_real_events = []
-        self.total_intra_syn_events = []
-        self.total_inter_events = []
+        # Set filename
+        self.filename = f"{self.eval_args.model_id}_{self.eval_args.syndata_id}"
 
-    def evaluate(self, files: List[str]):
+        # Set paths to dirs
+        self.path_scores = os.path.join(self.eval_dir, f"scores/")
+
+    def setup_folders(self):
+        # Create dirs
+        os.makedirs(self.path_scores, exist_ok=True)
+
+    def evaluate(self):
         print(f"Start diversity evaluation of model {self.eval_args.model_type}..")
-        return super().evaluate(files)
 
-    def _evaluate_dataset(self, syndata: torch.Tensor):
+        # Create dataframe
+        scores = pd.DataFrame()
+        scores["Model"] = pd.Series(self.eval_args.model_type)
+        scores = pd.concat([scores, self.evaluate_sequence_diversity()], axis=1)
+        scores = pd.concat([scores, self.evaluate_event_diversity()], axis=1)
+        
+        # # Store metric scores
+        path = os.path.join(self.path_scores, self.filename)
+        save_df_to_csv(scores, path)
+
+    def evaluate_sequence_diversity(self):
+        print(f"Evaluating sequence diversity..")
+
+        # Prepare data
+        train_data = self.eval_args.real_data[self.eval_args.train_data.indices]
+        
+        # Calculate metrics
+        rel_coverage_sequences, rel_diversity_sequences, intra_d_syn_sequences, intra_d_real_sequences, inter_d_sequences = calculate_diversity_scores(
+            train_data, self.syndata, self.eval_args.n_components, self.eval_args.n_neighbors_diversity, self.eval_args.coverage_factor, "events")
+
+        df = pd.DataFrame()
+        df["Rel. Diversity Seq"] = pd.Series(rel_diversity_sequences)
+        df["Rel. Coverage Seq"] = pd.Series(rel_coverage_sequences)
+        df["Intra Dist. Real Seq"] = pd.Series(intra_d_real_sequences)
+        df["Intra Dist. Syn. Seq"] = pd.Series(intra_d_syn_sequences)
+        df["Inter Dist. Seq"] = pd.Series(inter_d_sequences)
+
+        return df
+
+    def evaluate_event_diversity(self):
+        print(f"Evaluating event diversity..")
+
+        # Prepare data
         train_data = self.eval_args.real_data[self.eval_args.train_data.indices]
 
-        rel_coverage_sequences, rel_diversity_sequences, intra_d_syn_sequences, intra_d_real_sequences, inter_d_sequences = calculate_diversity_scores(
-            train_data, syndata, self.eval_args.n_components, self.eval_args.n_neighbors_diversity, self.eval_args.coverage_factor, "sequences")
-
+        # Calculate metrics
         rel_coverage_events, rel_diversity_events, intra_d_syn_events, intra_d_real_events, inter_d_events = calculate_diversity_scores(
-            train_data, syndata, self.eval_args.n_components, self.eval_args.n_neighbors_diversity, self.eval_args.coverage_factor, "events")
-        
-        self.total_rel_diversity_sequences.append(rel_diversity_sequences)
-        self.total_rel_coverage_sequences.append(rel_coverage_sequences)
-        self.total_intra_real_sequences.append(intra_d_real_sequences)
-        self.total_intra_syn_sequences.append(intra_d_syn_sequences)
-        self.total_inter_sequences.append(inter_d_sequences)
+            train_data, self.syndata, self.eval_args.n_components, self.eval_args.n_neighbors_diversity, self.eval_args.coverage_factor, "events")
 
-        self.total_rel_diversity_events.append(rel_diversity_events)
-        self.total_rel_coverage_events.append(rel_coverage_events)
-        self.total_intra_real_events.append(intra_d_real_events)
-        self.total_intra_syn_events.append(intra_d_syn_events)
-        self.total_inter_events.append(inter_d_events)
+        df = pd.DataFrame()
+        df["Rel. Diversity Seq"] = pd.Series(rel_diversity_events)
+        df["Rel. Coverage Seq"] = pd.Series(rel_coverage_events)
+        df["Intra Dist. Real Seq"] = pd.Series(intra_d_real_events)
+        df["Intra Dist. Syn. Seq"] = pd.Series(intra_d_syn_events)
+        df["Inter Dist. Seq"] = pd.Series(inter_d_events)
 
-
-    def _post_processing(self):
-        # Save non-averaged results to dataframe
-        non_avg_scores_df = pd.DataFrame()
-        non_avg_scores_df['Relative Sequence Diversity'] = pd.Series(self.total_rel_diversity_sequences)
-        non_avg_scores_df['Relative Sequence Coverage'] = pd.Series(self.total_rel_coverage_sequences)
-        non_avg_scores_df['Intra Distance Real Sequences'] = pd.Series(self.total_intra_real_sequences)
-        non_avg_scores_df['Intra Distance Syn Sequences'] = pd.Series(self.total_intra_syn_sequences)
-        non_avg_scores_df['Inter Distance Sequences'] = pd.Series(self.total_inter_sequences)
-
-        non_avg_scores_df['Relative Event Diversity'] = pd.Series(self.total_rel_diversity_events)
-        non_avg_scores_df['Relative Event Coverage'] = pd.Series(self.total_rel_coverage_events)
-        non_avg_scores_df['Intra Distance Real Events'] = pd.Series(self.total_intra_real_events)
-        non_avg_scores_df['Intra Distance Syn Events'] = pd.Series(self.total_intra_syn_events)
-        non_avg_scores_df['Inter Distance Events'] = pd.Series(self.total_inter_events)
-
-
-
-        return non_avg_scores_df
+        return df
